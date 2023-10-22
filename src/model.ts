544163bf2aedf4b66e8f979ffce1d6f7c6117761
max5961 <mmorrissey5961@gmail.com>
2023-10-22 18:17:44 -0400

    Clean shortestPath function

--- src/model.ts
+++ src/model.ts
@@ -1,16 +1,17 @@
 // prettier-ignore
-export function shortestPath(start: number[], dest: number[]): any {
+export function shortestPath(start: number[], dest: number[]): number[][] | any {
     const moves: number[][] = [
         [1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]
     ]
 
-    const arr: any = [start];
+    const queue = new Queue(start);
     const pathMap: any = {}
     const visited = new Set();
 
-    while (arr.length) {
-        const currCoord = arr.shift();
+    while (queue.size) {
+        const currCoord = queue.dequeue();
         const [x, y] = currCoord;
+
         if (x === dest[0] && y === dest[1]) {
             return getPath(pathMap, start, dest);
         }
@@ -23,7 +24,7 @@ export function shortestPath(start: number[], dest: number[]): any {
             if (isValid([nx, ny])) {
                 if (!visited.has([nx, ny].toString())) {
                     visited.add([nx, ny].toString());
-                    arr.push([nx, ny]);
+                    queue.enqueue([nx, ny]);
                     pathMap[[nx, ny].toString()] = [x, y];
                 }
             }
@@ -34,11 +35,12 @@ export function shortestPath(start: number[], dest: number[]): any {
 }
 
 function getPath(pathMap: any, start: number[], dest: number[]): number[][] {
-    const path: any = [dest];
+    const path = [dest];
     let curr = dest.toString();
-    while (curr.toString() !== start.toString()) {
-        path.unshift(pathMap[curr.toString()]);
-        curr = pathMap[curr.toString()];
+
+    while (curr !== start.toString()) {
+        path.unshift(pathMap[curr]);
+        curr = pathMap[curr].toString();
     }
 
     return path;
@@ -47,4 +49,41 @@ function getPath(pathMap: any, start: number[], dest: number[]): number[][] {
 function isValid(coord: number[]): boolean {
     const [x, y] = coord;
     return x >= 1 && x <= 8 && y >= 1 && y <= 8;
+}
+
+export class Queue {
+    items: [string: number[]] | any;
+    rear: number;
+    front: number;
+    size: number;
+
+    constructor(coord: number[] | null = null) {
+        this.items = {};
+        this.rear = 0;
+        this.front = 0;
+        this.size = 0;
+        this.init(coord);
+    }
+
+    init(coord: number[] | null = null): void {
+        if (coord) {
+            this.enqueue(coord);
+        }
+    }
+
+    enqueue(coord: number[] | null): void {
+        this.items[this.rear] = coord;
+        this.rear++;
+        this.size++;
+    }
+
+    dequeue(): number[] | any {
+        if (this.size) {
+            const coord = this.items[this.front];
+            delete this.items[this.front];
+            this.front++;
+            this.size--;
+            return coord;
+        }
+    }
 }
